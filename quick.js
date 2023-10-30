9128d362fe0a756ac3b3ff6ee25cb1e488a04525
Kevin Nichols <kevnichols2@gmail.com>
2023-10-30 10:32:36 -0400

    initial commit

---
+++ quick.js
@@ -0,0 +1,41 @@
+/*
+pivot accepts an array, starting index, and ending index
+You can assume the pivot is always the first element
+*/
+
+function pivot(arr, start = 0, end = arr.length - 1) {
+  function swap(arr, i, j) {
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+
+  const pivotValue = arr[start];
+  let pivotIndex = start;
+
+  for (let i = start + 1; i <= end; i++) {
+    if (arr[i] < pivotValue) {
+      pivotIndex++;
+      swap(arr, pivotIndex, i);
+    }
+  }
+
+  swap(arr, start, pivotIndex);
+
+  return pivotIndex;
+}
+
+/*
+quickSort accepts an array, left index, and right index
+*/
+
+function quickSort(arr, left = 0, right = arr.length - 1) {
+  if (left < right) {
+    const pivotIndex = pivot(arr, left, right);
+
+    quickSort(arr, left, pivotIndex - 1);
+    quickSort(arr, pivotIndex + 1, right);
+  }
+
+  return arr;
+}
+
+module.exports = {pivot, quickSort};
